d14b4cd68fd3fb66a3b8981cf90cdaf3609ff493
Tyler Moroso <tlmoroso@mit.edu>
2021-02-11 21:09:21 -0800

    Get bootyhole from backend

--- src/components/butt.js
+++ src/components/butt.js
@@ -4,10 +4,19 @@ class butt extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = { bootyhole: this.props.bootyhole }
+        this.state = {
+            bootyhole: this.props.bootyhole,
+            buttPhoto: null
+        }
     }
 
     render() {
+        // let buttURL = fetch("localhost:3000/butts/")
+        //     .then(response => {
+        //         console.log("Butt Photo: ", response);
+        //         this.state.buttPhoto = response;
+        //     });
+
         return(
             <div>
                 <h1>
@@ -16,6 +25,7 @@ class butt extends React.Component {
                 <p>
                     { this.state.bootyhole }
                 </p>
+                <img src={"localhost:3000/butts/"}  alt={"bootyhole"}/>
             </div>
         );
     }
